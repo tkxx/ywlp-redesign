78cdc966a9c0e82efb01e199e3b0f02769a315ed
User <user@Users-MBP.home>
2020-02-03 22:13:44 -0500

    routes page

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Homepage from "./components/Homepage/homepage";
 import About from "./components/About/about";
 import EventsPage from "./components/EventsPage/eventsPage";
-import ContactPage from "./components/ContactPage/contactpage";
+import Homepage from "./components/Homepage/homepage";
+import ContactUs from "./components/ContactPage/contactpage";
 
 export default (
   <Switch>
-    <Route path="/contactus" component={ContactPage} />
-    <Route path="/events/:id" component={EventsPage} />
     <Route path="/about" component={About} />
+    <Route path="/events/:id" component={EventsPage} />
+    <Route path="/contactus" component={ContactUs} />
     <Route exact path="/" component={Homepage} />
   </Switch>
 );
