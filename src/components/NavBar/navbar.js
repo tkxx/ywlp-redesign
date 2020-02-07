3bb7e56c53474067308b937afe50e048acbe14e1
User <26051646+tkxx@users.noreply.github.com>
2020-02-06 23:57:43 -0500

    still playing with navbar styling

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
     <React.Fragment>
-      <ul>
+      <ul class="navbar">
         <li>
           <Link to="/">Home</Link>
         </li>
